599d22f34bcaaa285128d0c7c537081e1c150c39
adithra-fintech <114465231+adithra-fintech@users.noreply.github.com>
2022-10-28 11:51:31 +0530

    Extra

--- src/components/LoginPart/LoginPart.js
+++ src/components/LoginPart/LoginPart.js
@@ -1,15 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './LoginPart.module.css';
+import { render } from '@testing-library/react';
+import Typography from '@mui/joy/Typography';
+import { Button, CssVarsProvider, Sheet, Link, TextField } from '@mui/joy';
 
-const LoginPart = () => (
-  <div className={styles.LoginPart} data-testid="LoginPart">
-    LoginPart Component
-  </div>
-);
-
+export default function LoginPart(props) {
+  return (
+    <CssVarsProvider>
+      <Sheet
+        sx={{
+          width: 300,
+          mx: 'auto', // margin left & right
+          my: 4, // margin top & botom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          borderRadius: 'sm',
+          boxShadow: 'md',
+        }}
+      >
+        <div>
+          <Typography level="h4" component="h1">
+            Welcome!
+          </Typography>
+          <Typography level="body2">Sign in to continue.</Typography>
+        </div>
+        <div>
+          <TextField
+            // html input attribute
+            name="email"
+            type="email"
+            placeholder="johndoe@email.com"
+            // pass down to FormLabel as children
+            label="Email"
+          />
+          <TextField
+            name="password"
+            type="password"
+            placeholder="password"
+            label="Password"
+          />
+        </div>
+        <div>
+          <Button sx={{ mt: 1 /* margin top */ }}>
+            Log in
+          </Button>
+          <Typography
+            endDecorator={<Link href="/sign-up">Sign up</Link>}
+            fontSize="sm"
+            sx={{ alignSelf: 'center' }}
+          >
+            Don't have an account?
+          </Typography>
+        </div>
+      </Sheet>
+    </CssVarsProvider>
+  );
+}
 LoginPart.propTypes = {};
 
 LoginPart.defaultProps = {};
-
-export default LoginPart;
