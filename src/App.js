ccab6e78f25440f27568b6e5d3521fb2c50fbe6c
adithra-fintech <114465231+adithra-fintech@users.noreply.github.com>
2022-10-28 11:51:09 +0530

    Form completed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import SimpleForm from './components/simpleForm/simpleForm';
-import { Button, CssVarsProvider, Sheet } from '@mui/joy';
-import Typography from '@mui/joy/Typography';
+import LoginPart from './components/LoginPart/LoginPart';
 
 function App() {
   return (
-    <div>
-      <CssVarsProvider>
-        <Sheet
-          sx={{
-            width: 300,
-            mx: 'auto', // margin left & right
-            my: 4, // margin top & botom
-            py: 3, // padding top & bottom
-            px: 2, // padding left & right
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 2,
-            borderRadius: 'sm',
-            boxShadow: 'md',
-          }}
-        >
-          <div>
-            <Typography level="h4" component="h1">
-              Welcome!
-            </Typography>
-            <Typography level="body2">Sign in to continue.</Typography>
-          </div>
-
-        </Sheet>
-      </CssVarsProvider>
-    </div>
+    <LoginPart></LoginPart>
   );
 }
 
