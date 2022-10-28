cecefc50000b980a69e59cfda55db2a4834f8a3c
adithra-fintech <114465231+adithra-fintech@users.noreply.github.com>
2022-10-28 12:55:31 +0530

    hf

--- src/components/LoginPart/LoginPart.js
+++ src/components/LoginPart/LoginPart.js
@@ -6,56 +6,56 @@ import ModeToggle from '../ModeToggle/ModeToggle';
 export default function LoginPart(props) {
   return (
     <CssVarsProvider>
-      <ModeToggle></ModeToggle>
-      <Sheet
-        sx={{
-          width: 300,
-          mx: 'auto', // margin left & right
-          my: 4, // margin top & botom
-          py: 3, // padding top & bottom
-          px: 2, // padding left & right
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          borderRadius: 'sm',
-          boxShadow: 'md',
-        }}
-      >
-        <div>
-          <Typography level="h4" component="h1">
-            Welcome!
-          </Typography>
-          <Typography level="body2">Sign in to continue.</Typography>
-        </div>
-        <div>
-          <TextField
-            // html input attribute
-            name="email"
-            type="email"
-            placeholder="johndoe@email.com"
-            // pass down to FormLabel as children
-            label="Email"
-          />
-          <TextField
-            name="password"
-            type="password"
-            placeholder="password"
-            label="Password"
-          />
-        </div>
-        <div>
-          <Button sx={{ mt: 1 /* margin top */ }}>
-            Log in
-          </Button>
-          <Typography
-            endDecorator={<Link href="/sign-up">Sign up</Link>}
-            fontSize="sm"
-            sx={{ alignSelf: 'center' }}
-          >
-            Don't have an account?
-          </Typography>
-        </div>
-      </Sheet>
+      <main>
+        <ModeToggle />
+        <Sheet
+          sx={{
+            width: 300,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & botom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+          }}
+        >
+          <div>
+            <Typography level="h4" component="h1">
+              Welcome!
+            </Typography>
+            <Typography level="body2">Sign in to continue.</Typography>
+          </div>
+          <div>
+            <TextField
+              // html input attribute
+              name="email"
+              type="email"
+              placeholder="johndoe@email.com"
+              // pass down to FormLabel as children
+              label="Email"
+            />
+            <TextField
+              name="password"
+              type="password"
+              placeholder="password"
+              label="Password"
+            />
+          </div>
+            <Button sx={{ mt: 1 /* margin top */ }}>
+              Log in
+            </Button>
+            <Typography
+              endDecorator={<Link href="/sign-up">Sign up</Link>}
+              fontSize="sm"
+              sx={{ alignSelf: 'center' }}
+            >
+              Don't have an account?
+            </Typography>
+        </Sheet>
+      </main>
     </CssVarsProvider>
   );
 }
